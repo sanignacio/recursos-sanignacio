1fb927a27e964db144882c5bc19784143686a19f
0-Sandy <101663779+0-Sandy@users.noreply.github.com>
2025-08-30 22:54:05 -0300

    refactor: better index (/) page (with animations!)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,33 @@
 import { ArrowRightIcon } from "@radix-ui/react-icons";
-import { ShieldCheck } from "lucide-react";
 import Link from "next/link";
 
 import { SignInButton } from "~/components/auth/sign-in-button";
 import { Footer } from "~/components/footer";
 import { Button } from "~/components/ui/button";
+import { FlipWords } from "~/components/ui/flip-words";
 
 export default function HomePage() {
+  const words = [
+    "imprime",
+    "encarga",
+    "organiza",
+    "solicita",
+    "gestiona",
+    "reserva",
+    "coordina",
+    "envía",
+  ];
+
   return (
     <>
       <main className="flex h-full min-h-[calc(100vh-36px-48px)] flex-col items-center justify-center">
         <section className="space-y-6 pt-6 pb-8 md:pt-10 md:pb-12 lg:py-32">
           <div className="container flex max-w-5xl flex-col items-center gap-4 text-center">
-            <Link href="/" className="mb-5 flex items-center">
-              <ShieldCheck
-                strokeWidth={2.5}
-                className="mr-1 h-auto w-10 md:w-14 lg:w-16"
-              />
-              <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl">
-                Recursos San Ignacio
-              </h1>
-            </Link>
-            <h1 className="text-3xl font-semibold sm:text-4xl md:text-5xl lg:text-6xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <div className="mb-5 text-5xl font-bold md:text-6xl lg:text-7xl">
+              San Ignacio <FlipWords words={words} />
+            </div>
+            <h1 className="text-xl font-semibold sm:text-xl md:text-2xl lg:text-3xl">
+              Con Recursos San Ignacio es mas fácil
             </h1>
             <p className="text-muted-foreground max-w-2xl leading-normal sm:text-lg sm:leading-8">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
